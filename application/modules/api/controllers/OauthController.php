a7bfe7d537e7315d41d539af037edd55155576ec
Kévin DUBUC <kevindubuc62@gmail.com>
2013-01-28 12:03:10 +0100

    addition of comments in oauth file

--- application/modules/api/controllers/OauthController.php
+++ application/modules/api/controllers/OauthController.php
@@ -3,26 +3,51 @@
 class Api_OauthController extends Zend_Controller_Action
 {
 
+    /**
+    * OAuthProvider object
+    */
+    private $provider;
+    
+    /**
+    * Boolean wich care of error statement
+    */
+    private $oauth_error;
+
+    /**
+    * Initialize the controller
+    */
     public function init()
     {
         /* Initialize action controller here */
     }
 
+    /**
+    * Index action
+    */
     public function indexAction()
     {
         // action body
     }
 
+    /**
+    * Request token endpoint
+    */
     public function requestTokenAction()
     {
         // action body
     }
 
+    /**
+    * Access token endpoint
+    */
     public function accessTokenAction()
     {
         // action body
     }
 
+    /**
+    * Authorize action
+    */
     public function authorizeAction()
     {
         // action body
