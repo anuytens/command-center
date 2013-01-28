ad5d26a2218595a9e9100710c2dd6a0416f91a22
Vanlaere Charly <vanlaere.charly@gmail.com>
2013-01-28 15:03:45 +0100

    login form

--- application/forms/Login.php
+++ application/forms/Login.php
@@ -5,8 +5,28 @@ class Application_Form_Login extends Zend_Form
 
     public function init()
     {
-        /* Form Elements & Other Definitions Here ... */
+        $this->setMethod("post");
+    	
+    	
+        $username = new Zend_Form_Element_Text('username');
+        $username->addFilter(new Zend_Filter_HtmlEntities);
+        $username->addValidators(array(new Zend_Validate_StringLength(1,255) , new Zend_Validate_Alpha));
+        $username->setLabel("Username : ");
+        $username->setRequired(true);
+        $this->addElement($username);
+
+		$password = new Zend_Form_Element_Password('password');
+        $password->addFilter(new Zend_Filter_HtmlEntities);
+        $password->addValidators(array(new Zend_Validate_StringLength(1,255)));
+        $password->setLabel("Password : ");
+        $password->setRequired(true);
+        $this->addElement($password);
+		
+        //bouton de validation
+        $submit = new Zend_Form_Element_Submit('Se connecter');
+        $this->addElement($submit);
     }
+    
 
 
 }
