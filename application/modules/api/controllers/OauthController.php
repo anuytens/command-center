110e9013920b92cac505eebf0b321e7b4cd1bbd9
Kévin DUBUC <kevindubuc62@gmail.com>
2013-01-28 11:18:38 +0100

    add the api structure with oauth controller

---
+++ application/modules/api/controllers/OauthController.php
@@ -0,0 +1,39 @@
+<?php
+
+class Api_OauthController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function requestTokenAction()
+    {
+        // action body
+    }
+
+    public function accessTokenAction()
+    {
+        // action body
+    }
+
+    public function authorizeAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
+
+
