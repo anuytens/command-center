f49576fefe44d0546f82a376f79ff3b071b75df2
Kévin DUBUC <kevindubuc62@gmail.com>
2013-01-29 11:48:17 +0100

    autoload for views and action helpers

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,28 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+    public function run()
+    {
+		$this->loadActionHelpers();
+		$this->loadViewHelpers();
+		parent::run();
+	}
 
-
+	public function loadActionHelpers()
+    {
+		Zend_Controller_Action_HelperBroker::addPath(
+			APPLICATION_PATH . DIRECTORY_SEPARATOR ."controllers" . DIRECTORY_SEPARATOR . "helpers",
+			"Application_Controller_Helper_"
+		);
+	}
+	
+	public function loadViewHelpers()
+    {
+		$layout = $this->getResource("layout");
+		$view = $layout->getView();
+		$view->setHelperPath(
+            APPLICATION_PATH . DIRECTORY_SEPARATOR ."views" . DIRECTORY_SEPARATOR . "helpers",
+            'Application_View_Helper_'
+        );
+	}
 }
-
