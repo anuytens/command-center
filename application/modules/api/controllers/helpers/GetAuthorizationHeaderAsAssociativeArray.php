ae18fb314e876ec0a0ea29a46901739ca149b0e9
Kévin DUBUC <kevindubuc62@gmail.com>
2013-01-31 16:11:41 +0100

    add action helper to parse in array authorization

---
+++ application/modules/api/controllers/helpers/GetAuthorizationHeaderAsAssociativeArray.php
@@ -0,0 +1,26 @@
+<?php
+
+class Api_Controller_Helper_GetAuthorizationHeaderAsAssociativeArray extends Zend_Controller_Action_Helper_Abstract
+{
+    public function direct()
+    {
+        $result = array();
+    
+        // get Authorization header
+        $header = $this->getRequest()->getHeader("Authorization");
+        
+        // explode by ","
+        $headerValues = explode(",", $header);
+        
+        // parse
+        foreach($headerValues as $headerValue)
+        {
+            $key = strstr($headerValue, "=", true);
+            preg_match("/$key=\"(.*)\"/", $headerValue, $value);
+            
+            $result[$key] = $value[1];
+        }
+        
+        return $result;
+    }
+}
