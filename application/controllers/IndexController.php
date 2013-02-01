31df09a1e4cfe79c09e632993c2afdcedc0f4a7b
Kévin DUBUC <kevindubuc62@gmail.com>
2013-02-01 15:48:00 +0100

    update index controller

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -11,11 +11,29 @@ class IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
+        $auth = Zend_Auth::getInstance();
+        
+        Zend_Debug::Dump($auth->getIdentity());
     }
 
-    public function authenticateAction()
+    public function loginAction()
     {
-        // action body
+        // get the login form
+        $form_login = new Application_Form_Login;
+        
+        // Check if there is a request
+        if($this->_request->isPost() && $form_login->isValid($this->_request->getPost()))
+        {
+            // perform login
+            $this->_helper->performLdapLogin(
+                $this->_request->getPost("username"),
+                $this->_request->getPost("password")
+            );
+            
+            $this->_helper->redirector("index");
+        }
+        
+        $this->view->form = $form_login;
     }
 
     public function logoutAction()
@@ -25,6 +43,9 @@ class IndexController extends Zend_Controller_Action
         
         // clear the identity
         $auth->clearIdentity();
+        
+        // redirect to index
+        $this->_helper->redirector("index");
     }
 
 
