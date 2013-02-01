7f2f6896d6d7e0e6bf0d074a322ec972406bf993
Kévin DUBUC <kevindubuc62@gmail.com>
2013-02-01 16:57:18 +0100

    update user controller

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -2,40 +2,59 @@
 
 class IndexController extends Zend_Controller_Action
 {
-
+    /**
+     * Initialize the controller
+     */
     public function init()
     {
         /* Initialize action controller here */
     }
-
+    
+    /**
+     * index
+     */
     public function indexAction()
     {
         // action body
-        $auth = Zend_Auth::getInstance();
-        
-        Zend_Debug::Dump($auth->getIdentity());
     }
-
+    
+    /**
+     * Log in the user
+     */
     public function loginAction()
     {
-        // get the login form
-        $form_login = new Application_Form_Login;
-        
-        // Check if there is a request
-        if($this->_request->isPost() && $form_login->isValid($this->_request->getPost()))
+        // get the auth instance
+        $auth = Zend_Auth::getInstance();
+    
+        // if the user is authenticated, redirect to index action
+        if(!$auth->hasIdentity())
         {
-            // perform login
-            $this->_helper->performLdapLogin(
-                $this->_request->getPost("username"),
-                $this->_request->getPost("password")
-            );
+            // get the login form
+            $form_login = new Application_Form_Login;
             
+            // Check if there is a request
+            if($this->_request->isPost() && $form_login->isValid($this->_request->getPost()))
+            {
+                // perform login
+                $this->_helper->performLdapLogin(
+                    $this->_request->getPost("username"),
+                    $this->_request->getPost("password")
+                );
+                
+                $this->_helper->redirector("index");
+            }
+            
+            $this->view->form = $form_login;
+        }
+        else
+        {
             $this->_helper->redirector("index");
         }
-        
-        $this->view->form = $form_login;
     }
 
+    /**
+     * Log out the user
+     */
     public function logoutAction()
     {
         // get the instance of auth
